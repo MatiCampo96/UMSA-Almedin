d7c84d6efb40bcc4afb40f0f58d61923e30082c6
Nicolas Ippoliti <91435048+NicolasIppoliti@users.noreply.github.com>
2024-06-18 17:45:19 -0300

    npm run dev con --host

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // Asegura que el servidor sea accesible externamente
-    port: 5173,       // Especifica el puerto en el que se ejecuta tu aplicación
-  },
 })
