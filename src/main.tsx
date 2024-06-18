dcaa65a4493fdccd0d81f151e10cb50bfb9fabe9
Nicolas Ippoliti <91435048+NicolasIppoliti@users.noreply.github.com>
2024-06-18 13:26:22 -0300

    MUI config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@mui/material/styles';
+import App from './App';
+import theme from './theme';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
