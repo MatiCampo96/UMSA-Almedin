4324d0a09039d1035253c6f0d54d4eee57148efb
Nicolas Ippoliti <91435048+NicolasIppoliti@users.noreply.github.com>
2024-06-18 13:19:48 -0300

    linteo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1>Trabajo Integrador Grupo 10 - Frontend</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
