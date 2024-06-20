a5494ff72af3840970ad10fc980c98610fd58d79
MatiCampo96 <maticampopiano@hotmail.com>
2024-06-20 04:32:53 -0300

    Cartilla medica conectada y funcional, pero sencilla esteticamente

--- src/components/SpecialistCard.tsx
+++ src/components/SpecialistCard.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
 import { Card, CardContent, Typography } from '@mui/material';
+import { Schedule } from '../types';
+import { formatSchedule, sortSchedulesByDay } from '../utils/schedule';
+
 
 interface SpecialistCardProps {
-  name: string;
+  firstName: string;
+  lastName: string;
   specialty: string;
-  schedule: string;
   location: string;
+  schedule: Schedule[];
 }
 
-const SpecialistCard: React.FC<SpecialistCardProps> = ({ name, specialty, schedule, location }) => {
+const SpecialistCard: React.FC<SpecialistCardProps> = ({ firstName, lastName, specialty, location, schedule }) => {
+  const formattedSchedules = schedule.map(formatSchedule);
+  const sortedSchedules = sortSchedulesByDay(formattedSchedules);
+
+  const formatSpecialty = (specialty: string): string => {
+    return specialty.replace(/_/g, ' ');
+  };
   return (
     <Card sx={{ maxWidth: 345, mb: 2 }}>
       <CardContent>
-        <Typography variant="h6">{name}</Typography>
-        <Typography color="text.secondary">{specialty}</Typography>
-        <Typography color="text.secondary">{schedule}</Typography>
+        <Typography variant="h6">{firstName + " " + lastName}</Typography>
+        <Typography color="text.secondary">{formatSpecialty(specialty)}</Typography>
         <Typography color="text.secondary">{location}</Typography>
+        <Typography color="text.secondary">
+          {sortedSchedules.map(formattedSchedule => (
+            <span key={formattedSchedule.id}>
+              {formattedSchedule.day} {formattedSchedule.entryTime}-{formattedSchedule.departureTime}<br />
+            </span>
+          ))}
+        </Typography>
       </CardContent>
     </Card>
   );
--- src/pages/SpecialistList.tsx
+++ src/pages/SpecialistList.tsx
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Typography, Grid } from '@mui/material';
 import SpecialistCard from '../components/SpecialistCard';
+import { Specialist } from '../types';
 
-const specialists = [
-  //TODO: Logica de la lista de especialistas
-  // Especialistas hardcodeados
-  { name: 'Dr. Juan Perez', specialty: 'Cardiología', schedule: 'Lunes a Viernes 9:00 - 17:00', location: 'Sucursal Centro' },
-  { name: 'Dra. María Lopez', specialty: 'Dermatología', schedule: 'Lunes a Jueves 10:00 - 16:00', location: 'Sucursal Norte' },
-  { name: 'Dr. Pedro Rodriguez', specialty: 'Pediatria', schedule: 'Lunes a Sabado 10:00 - 18:00', location: 'Sucursal Sur' },];
+const ListSpecialists: React.FC = () => {
+  const [specialists, setSpecialists] = useState<Specialist[]>([]);
+  const [error, setError] = useState<string | null>(null);    //Puede usarse un Loading en lugar de error, error no esta implementado...
 
-const SpecialistList: React.FC = () => {
+  useEffect(() => {
+    console.log('Fetching doctors...');
+    fetch('http://localhost:8080/especialistas')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Data received:', data);  //Opcional, util durante depuración
+        setSpecialists(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error); //Opcional, util durante depuración
+        setError(error.message);
+      });
+  }, []);
+
   return (
     <Container maxWidth="lg">
       <Typography variant="h4" component="h1" gutterBottom>
         Cartilla de Especialistas
       </Typography>
       <Grid container spacing={4}>
-        {specialists.map((specialist, index) => (
-          <Grid item key={index} xs={12} sm={6} md={4}>
+        {specialists.map((specialist) => (
+          <Grid item key={specialist.id} xs={12} sm={6} md={4}>
             <SpecialistCard
-              name={specialist.name}
-              specialty={specialist.specialty}
-              schedule={specialist.schedule}
-              location={specialist.location}
+              firstName={specialist.firstName}
+              lastName={specialist.lastName}
+              specialty={specialist.speciality}
+              location={specialist.branch.city}
+              schedule={specialist.schedules}
             />
           </Grid>
         ))}
@@ -31,4 +48,4 @@ const SpecialistList: React.FC = () => {
   );
 };
 
-export default SpecialistList;
+export default ListSpecialists;
