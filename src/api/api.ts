ba16b930c0f85daf6b0bf489370fb68a82ceb4b9
MatiCampo96 <maticampopiano@hotmail.com>
2024-06-25 22:26:01 -0300

    Login sencillo funcionando

--- src/api/api.ts
+++ src/api/api.ts
@@ -9,3 +9,26 @@ export const fetchSpecialists = async (): Promise<Specialist[]> => {
   const response = await api.get("/especialistas");
   return response.data;
 };
+
+// Función para iniciar sesión
+export const login = async (email: string, password: string): Promise<string> => {
+  try {
+    const response = await api.get('/auth/login', {
+      params: { email, password },
+    });
+    return response.data; // Suponiendo que el token es devuelto como la data de la respuesta
+  } catch (error) {
+    handleAxiosError(error);
+    throw error; // Lanzar el error después de manejarlo
+  }
+};
+
+
+// Función para manejar errores de Axios
+const handleAxiosError = (error: any) => {
+  if (axios.isAxiosError(error) && error.response) {
+    throw new Error(error.response.data);
+  } else {
+    throw new Error('An unexpected error occurred');
+  }
+};
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
-import { Container, Typography, TextField, Button, Box } from '@mui/material';
+import React, { useState } from 'react';
+import { login } from '../api/api';
+import { useNavigate } from 'react-router-dom';
+import { Container, Typography, TextField, Button, Box, Alert } from '@mui/material';
 
 const Login: React.FC = () => {
   //TODO: Logica de iniciar sesion
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
+
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const token = await login(email, password);
+      localStorage.setItem('token', token);  // Almacenar el token
+      navigate('/dashboard');  // Redirigir al usuario
+    } catch (error: any) {
+      setError(error.message);
+    }
+  };
+
   return (
     <Container maxWidth="sm">
       <Box sx={{ mt: 8 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Iniciar Sesion
         </Typography>
-        <form noValidate>
+        <form noValidate onSubmit={handleSubmit}>
+        {error && <Alert severity="error">{error}</Alert>}
           <TextField
             margin="normal"
             required
@@ -19,6 +48,8 @@ const Login: React.FC = () => {
             name="email"
             autoComplete="email"
             autoFocus
+            value={email}
+            onChange={handleEmailChange}
           />
           <TextField
             margin="normal"
@@ -29,6 +60,8 @@ const Login: React.FC = () => {
             type="password"
             id="password"
             autoComplete="current-password"
+            value={password}
+            onChange={handlePasswordChange}
           />
           <Button
             type="submit"
